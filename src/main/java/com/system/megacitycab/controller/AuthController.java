40f388f9ee9f46380c2381268f1a49f1fa7bc0f7
Uvindu28 <uvindu03@gmail.com>
2025-02-25 14:01:32 +0530

    Updated the Driver controller

--- src/main/java/com/system/megacitycab/controller/AuthController.java
+++ src/main/java/com/system/megacitycab/controller/AuthController.java
@@ -6,14 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.system.megacitycab.util.JwtUtil;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
--- src/main/java/com/system/megacitycab/model/Driver.java
+++ src/main/java/com/system/megacitycab/model/Driver.java
@@ -33,4 +33,6 @@ public class Driver {
 
     private boolean hasOwnCar = false;
 
+    private String profileImage;
+
 }
